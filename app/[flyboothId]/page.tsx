1aa4f8d8e3e38cd4a18f35280bf73feece0a8ea6
alexis <alexis.anzieu@gmail.com>
2024-03-12 16:05:07 -0400

    only on new flybooth

--- app/[flyboothId]/page.tsx
+++ app/[flyboothId]/page.tsx
@@ -161,7 +161,7 @@ export default function Index({ params: { flyboothId } }: Readonly<PageProps>) {
 
   return (
     <Box bgColor={"purple"} minH={"100vh"}>
-      <Fireworks autorun={{ speed: 3, duration: 1000 }} />
+      {!hasSavedLink && <Fireworks autorun={{ speed: 3, duration: 1000 }} />}
       <Container maxW="3xl" textAlign={"center"} color={"white"} pt={10}>
         <Heading size={"xl"}>{"  Bienvenue dans ton flybooth !"}</Heading>
         <Box className="step" borderRadius={20} pb="10" boxShadow={"2xl"}>
